2fbe60199aac03b6d999b14336dad8fa54fb0052
Fer Torres <fertorres638gmail.com>
2023-03-05 13:27:46 -0600

    creación de build

---
+++ src/dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+
+function Saludo() {
+    const usuario = {
+        nombre: TextInput,
+        apellido: TextInput
+    }
+}
