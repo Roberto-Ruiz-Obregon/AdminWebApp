70fe189a5845276c5f57a2de2aceea514bbf85a1
felix-rojas <a01201946@tec.mx>
2023-03-08 09:53:31 -0600

    add VerCursos template

--- src/screens/verCursos.js
+++ src/screens/verCursos.js
@@ -1,6 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
 import '../styles/style.css';
 
-export function VerCursos() {
-    <h1>Aquí verás los cursos subidos</h1>
+
+class VerCursos extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cursos:  [],
+            isLoaded: true
+        }
+    }
+    
+    getAllCourses() {
+        fetch(URL)
+        .then((res) => res.json())
+        .then((json) => {
+            this.setState({
+                cursos: json,
+                isLoaded: true
+            });
+        })
+    }
+    
+    render() {
+        const { isLoaded, cursos } = this.state;
+        if (!isLoaded) return <div>
+            <h1> Por favor espere... </h1> </div> ;
+
+        return (
+        <div className = "Cursos">
+            <h1> Cursos subidos </h1>  {
+                cursos.map((curso) => ( 
+                    <ol key = { curso.id } >
+                    nombreCurso: { curso.nombreCurso }, 
+                    fechaInicio: { curso.fechaInicio }, 
+                    ponente: { curso.ponente } 
+                    </ol>
+                ))
+            }
+        </div>
+    );}
 }
+
+export default VerCursos;
