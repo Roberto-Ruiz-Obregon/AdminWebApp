1391f1175b60ec67fb0d44095de58c1237926a35
felix-rojas <a01201946@tec.mx>
2023-03-08 08:55:27 -0600

    add firebase call

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,17 +1,29 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
 
+// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_FIREBASE_APPID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
-};
+      apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+      authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+      projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+      storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+      messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+      appId: process.env.REACT_APP_FIREBASE_APPID,
+      measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENTID
+    };
 
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+// doc reference
+// // Get a list of cities from your database
+// async function getCities(db) {
+//   const citiesCol = collection(db, 'cities');
+//   const citySnapshot = await getDocs(citiesCol);
+//   const cityList = citySnapshot.docs.map(doc => doc.data());
+//   return cityList;
+// }
+
+
