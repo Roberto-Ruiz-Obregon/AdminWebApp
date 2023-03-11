3cd2502cdc78538a6dd8caad75b0f7fc9b794fef
Fer Torres <fertorres638gmail.com>
2023-03-11 12:24:40 -0600

    vista agregar cursos

--- src/screens/cursos.js
+++ src/screens/cursos.js
@@ -30,18 +30,23 @@ function Cursos() {
       <h2 className="agregarc">Agregar Cursos</h2>
 
       <form onSubmit={handleSubmit} id="form">
-        <label htmlFor="newCourse" id="newCourse_">
+        <label htmlFor="newCourse" className="newCourse_">
           Título del curso:
         </label>
-        <input type="text" id="newCourse" className="newCourse_inpt" value={newCourse} onChange={handleInputChange} />
-        <button type="submit" id="agregar">
+        <input type="text" id="newCourse" className="newCourse_inpt" value={newCourse} onChange={handleInputChange} placeholder="Manualidades 3B"/>
+        <h6 className="descripcion">Descripción del curso:</h6>
+        <input type="text" className="description" onChange={handleInputChange} placeholder="En el curso podrás aprender..."/>
+      </form>
+      <label className="fecha">Fecha</label>
+      <label className="cargo">Cargo</label>
+      <label className="modalidad">Modalidad</label>
+      <button type="submit" className="agregar">
           Agregar Curso
         </button>
-      </form>
-     
       {courses.map((course, index) => ( 
         <CourseCard key={index} courseName={course}/>
       ))}
+      
     </div>
   );
 }
