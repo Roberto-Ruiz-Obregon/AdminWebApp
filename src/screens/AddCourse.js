b1bd6ee149d4a1d2adb5419f4e0b8dd502fc2d98
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-03-25 02:15:23 -0600

    Added basic inputs for course creation

--- src/screens/AddCourse.js
+++ src/screens/AddCourse.js
@@ -1,32 +1,32 @@
 import React, { useState, useEffect } from 'react';
 import CourseCard from '../components/CourseCard';
 import Input from '../components/Input';
+import Select from '../components/Select';
 import Button from '../components/Button';
 import '../styles/addCourse.css';
 
-function Cursos() {
+function AddCourse() {
     // Course attributes
     const [courseName, setCourseName] = useState('');
     const [description, setDescription] = useState('');
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
     const [schedule, setSchedule] = useState('');
     const [teacher, setTeacher] = useState('');
     const [capacity, setCapacity] = useState('');
-    const [accessLink, setAccessLink] = useState('');
-    const [cost, setCost] = useState(0);
     const [postalCode, setPostalCode] = useState('');
     const [image, setImage] = useState(null);
 
     const [modality, setModality] = useState('');
-    const [occupation, setOccupation] = useState('');
+    const [accessLink, setAccessLink] = useState('');
+    const [address, setAddress] = useState('');
+    const [status, setStatus] = useState('');
+    const [cost, setCost] = useState(0);
 
     // Topics
     const [topicsInCourse, setTopicsInCourse] = useState([]);
     const [topicsInCourseUI, setTopicsInCourseUI] = useState([]);
     const [topics, setTopics] = useState([]);
-
-    const [course, setCourse] = useState({});
 
     return (
         <div className='add-course-container'>
@@ -34,8 +34,117 @@ function Cursos() {
             <h2 className=''>Agregar Cursos</h2>
             <div className='form-preview-container'>
                 <form className='form-container'>
-                    <Input label='Titulo del curso' placeholder='Curso de lectura' />
-
+                    <Input
+                        label='Titulo del curso'
+                        placeholder='Curso de lectura'
+                        getVal={courseName}
+                        setVal={setCourseName}
+                        type='text'
+                    />
+                    <Input
+                        label='Descripcion del curso'
+                        placeholder='En este curso...'
+                        getVal={description}
+                        setVal={setDescription}
+                        type='text'
+                    />
+                    <Input
+                        label='Fecha de inicio del curso'
+                        placeholder=''
+                        getVal={startDate}
+                        setVal={setStartDate}
+                        type='date'
+                    />
+                    <Input
+                        label='Fecha de fin del curso'
+                        placeholder=''
+                        getVal={endDate}
+                        setVal={setEndDate}
+                        type='date'
+                    />
+                    <Input
+                        label='Horario del curso'
+                        placeholder='Este curso se imparte a las 6:00pm los sabados'
+                        getVal={schedule}
+                        setVal={setSchedule}
+                        type='text'
+                    />
+                    <Input
+                        label='Maestro del curso'
+                        placeholder='Dr. Juan Villeda'
+                        getVal={teacher}
+                        setVal={setTeacher}
+                        type='text'
+                    />
+                    <Input
+                        label='Capacidad del curso'
+                        placeholder='20'
+                        getVal={capacity}
+                        setVal={setCapacity}
+                        type='number'
+                    />
+                    <Input
+                        label='Codigo postal del curso'
+                        placeholder='76159'
+                        getVal={postalCode}
+                        setVal={setPostalCode}
+                        type='text'
+                    />
+                    <Input
+                        label='Imagen de la portada'
+                        placeholder=''
+                        getVal={image}
+                        setVal={setImage}
+                        type='file'
+                    />
+                    <Select
+                        label='Selecciona la modalidad del curso'
+                        getVal={modality}
+                        setVal={setModality}
+                        options={['Remoto', 'Presencial']}
+                    />
+                    {modality === 'Remoto' ? (
+                        <Input
+                            label='Link de acceso del curso'
+                            placeholder='zoom.com'
+                            getVal={accessLink}
+                            setVal={setAccessLink}
+                            type='text'
+                        />
+                    ) : (
+                        <Input
+                            label='Direccion del curso'
+                            placeholder='Calle de la Revolucion #130'
+                            getVal={address}
+                            setVal={setAddress}
+                            type='text'
+                        />
+                    )}
+                    <Input
+                        label='Imagen de la portada'
+                        placeholder=''
+                        getVal={image}
+                        setVal={setImage}
+                        type='file'
+                    />
+                    <Select
+                        label='Selecciona el tipo de pago'
+                        getVal={status}
+                        setVal={setStatus}
+                        options={['Gratuito', 'Pagado']}
+                    />
+                    {status === 'Pagado' ? (
+                        <Input
+                            label='Costo'
+                            placeholder='150'
+                            getVal={cost}
+                            setVal={setCost}
+                            type='number'
+                        />
+                    ) : (
+                        <></>
+                    )}
+                    <h3 className=''>Intereses del curso</h3>
                     {/* <label className='cargo'>Costo</label>
                 <select
                     name='occupation'
@@ -60,18 +169,18 @@ function Cursos() {
                 </select> */}
                 </form>
                 <div className='course-container'>
-                    <CourseCard
-                        courseName={course.title}
-                        description={course.description}
-                        startDate={course.date}
-                        occupation={course.occupation}
-                        modality={course.modality}
-                        imgSrc={course.imgSrc}
-                    />
+                    {/* <CourseCard
+                        courseName={courseName}
+                        description={description}
+                        startDate={startDate}
+                        occupation={status}
+                        modality={modality}
+                        imgSrc={image}
+                    /> */}
                 </div>
             </div>
         </div>
     );
 }
 
-export default Cursos;
+export default AddCourse;
