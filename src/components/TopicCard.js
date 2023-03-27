f7b80c83ae26a0ec1fcef6d7f97446e054587c7c
CesarJimenezVilleda02 <A01703517@tec.mx>
2023-03-26 21:21:52 -0600

    Finished styling for inscriptions and implemented deletion functionality

--- src/components/TopicCard.js
+++ src/components/TopicCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from '../components/Button';
+import Button from './Button';
 import '../styles/topicCard.css';
 
 function TopicCard(props) {
--- src/screens/Inscriptions.js
+++ src/screens/Inscriptions.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 import { getCourse, getCourseInscriptions } from '../client/course';
 import { deleteInscription } from '../client/inscriptions';
 import { FireError, FireSucess, FireQuestion } from '../utils/alertHandler';
+import InscriptionCard from '../components/InscriptionCard';
 import '../styles/inscriptions.css';
 
 function Inscriptions() {
@@ -24,7 +25,7 @@ function Inscriptions() {
                 FireError(error.response.data.message);
             }
         })();
-    }, []);
+    }, [id]);
 
     const handleDelete = async (id) => {
         try {
@@ -37,7 +38,7 @@ function Inscriptions() {
 
             await deleteInscription(id);
 
-            // setTopics(topics.filter((topic) => topic._id !== id));
+            setInscriptions(inscriptions.filter((inscription) => inscription._id !== id));
 
             FireSucess('Se ha eliminado la inscripcion con exito.');
         } catch (error) {
@@ -46,9 +47,24 @@ function Inscriptions() {
     };
 
     return (
-        <div className='interest-container'>
+        <div className='inscriptions-container'>
             <h4>Inicio / inscripciones</h4>
             <h2>Inscripciones a {course.courseName}</h2>
+            <div className='inscription-list-titles'>
+                <div>Día de inscripción</div>
+                <div>Nombre</div>
+                <div>Correo</div>
+                <div>Edad</div>
+                <div>Nivel educativo</div>
+                <div>Código postal</div>
+                <div>Ocupación</div>
+                <div>Acciones</div>
+            </div>
+            <div className='inscription-list-container'>
+                {inscriptions.map((inscription) => (
+                    <InscriptionCard inscription={inscription} action={handleDelete} />
+                ))}
+            </div>
         </div>
     );
 }
