776d19f2e9980687f2bec2ebd55d18d729aab1bd
DenisseDB <A01702603@tec.mx>
2023-04-18 10:01:58 -0600

    avance de busqueda

--- src/screens/Courses.js
+++ src/screens/Courses.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect, Fragment } from 'react';
 import { useNavigate } from 'react-router-dom';
 import CourseCard from '../components/CourseCard';
 import { getCourses } from '../client/course';
+import { getTopics } from '../client/topics';
 import { Video, Users, Calendar } from 'react-feather';
 import Button from '../components/Button';
+import Select from '../components/Select';
 import Pagination from '../components/Pagination';
 import Input from '../components/Input';
 import '../styles/verCursos.css';
@@ -11,10 +13,14 @@ import '../styles/verCursos.css';
 function Courses() {
     const navigate = useNavigate();
     const [courses, setCourses] = useState([]);
+    const [getTopicsObjects, setTopicsObjects] = useState([])
+    const [getTopicsNames, setTopicsNames] = useState([])
     const [getPage, setPage] = useState(1);
     const [getName, setName] = useState("");
     const [getLength, setLength] = useState("");
     const [getPostalCode, setPostalCode] =  useState("");
+    const [getCurrentTopic, setCurrentTopic] =  useState("");
+    const [getModality, setModality] = useState("");
 
     useEffect(() => {
         getCourses()
@@ -22,15 +28,32 @@ function Courses() {
             setCourses(data)
             setLength(data.length)
         });
+        getTopics()
+        .then(data => {
+            setTopicsObjects(data)
+            setTopicsNames(data.map(topic => topic.topic))
+        });
     }, []);
 
     useEffect(() => {
-        getCourses(getName, getPage, 8, '', getPostalCode)
+        getCourses(getName, getPage, 8, getCurrentTopic, getPostalCode, getModality)
         .then(data => {
             setCourses(data)
             setLength(data.length)
         });
-    }, [getPage, getPostalCode, getName]);
+    }, [getPage, getPostalCode, getName, getModality]);
+
+    useEffect(() => {
+        let topicId = ''
+        getTopicsObjects.forEach(topic => {
+            if(topic.topic == getCurrentTopic) topicId = topic._id
+        })
+        getCourses(getName, getPage, 8, topicId, getPostalCode)
+        .then(data => {
+            setCourses(data)
+            setLength(data.length)
+        });
+    }, [getCurrentTopic]);
 
     return (
         <Fragment>
@@ -41,7 +64,7 @@ function Courses() {
             <div className='search-container'>
                 <div className='input-container'>
                     <Input
-                        label='Buscar curso por nombre:'
+                        label='Nombre del curso a buscar:'
                         placeholder = "Buscar"
                         getVal={getName}
                         setVal={setName}
@@ -50,7 +73,7 @@ function Courses() {
                 </div>
                 <div className='input-container'>
                     <Input
-                        label='Buscar curso por CP:'
+                        label='Código postal de curso:'
                         placeholder = "12345"
                         getVal={getPostalCode}
                         setVal={setPostalCode}
@@ -58,6 +81,28 @@ function Courses() {
                     />
                 </div>
             </div>
+
+            <div className='search-container'>
+                <div className='input-container'>
+                <Select
+                        className='input-general'
+                        label='Selecciona un interés'
+                        getVal={getCurrentTopic}
+                        setVal={setCurrentTopic}
+                        options={getTopicsNames}
+                    />
+                </div>
+                <div className='input-container'>
+                    <Select
+                        label='Selecciona la modalidad del curso'
+                        getVal={getModality}
+                        setVal={setModality}
+                        options={['Remoto', 'Presencial']}
+                    />
+                </div>
+            </div>
+
+
             <div id="course-container">
                 { courses.map(course => (
                     <CourseCard 
