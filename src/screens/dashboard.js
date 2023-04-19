f6a525780b71ec5e17d909f545b4cb4aad842d11
Fer Torres <fertorres638gmail.com>
2023-04-18 18:25:00 -0600

    grafica zonas más usuarios

--- src/screens/dashboard.js
+++ src/screens/dashboard.js
@@ -1,154 +1,84 @@
-import React from 'react';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
 } from 'chart.js';
 import { Chart } from 'react-chartjs-2';
+import axios from 'axios';
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
 );
 
 const Dashboard = () => {
-    const [getChartData, setChartData] = useState({
-        labels: [],
+  const [getChartData, setChartData] = useState({
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        label: 'Zonas con más usuarios',
+        borderColor: '#3333ff',
+        fill: true,
+        lineTension: 0.1,
+      },
+    ],
+  });
+  const options = {
+    responsive: false,
+    maintainAspectRatio: false,
+    plugins: {
+      legend: {
+        position: 'top',
+        labels: {
+          font: {
+            size: 5,
+            color: 'rgba(255, 255, 255, 1)',
+          },
+        },
+      },
+    },
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get('/api/zones-most-users');
+      const data = result.data.data;
+      const labels = data.map((zone) => zone.postalCode);
+      const counts = data.map((zone) => zone.totalUsers);
+      setChartData({
+        labels: labels,
         datasets: [
           {
-            data: [],
-            label: 'Intereses por zona',
-            backgroundColor: [
-              '#FF6384',
-              '#36A2EB',
-              '#FFCE56',
-              '#FF6384',
-              '#36A2EB',
-              '#FFCE56'
-            ],
-            hoverBackgroundColor: [
-              '#FF6384',
-              '#36A2EB',
-              '#FFCE56',
-              '#FF6384',
-              '#36A2EB',
-              '#FFCE56'
-            ]
-          }
-        ]
+            data: counts,
+            label: 'Zonas con más usuarios',
+            borderColor: '#3333ff',
+            fill: true,
+            lineTension: 0.1,
+          },
+        ],
       });
-      
-      useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await fetch('/filter-topics', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify({ postalCode: '76158' }) 
-            });
-      
-            const data = await response.json();
-      
-            const labels = data.data.map(item => item._id);
-            const counts = data.data.map(item => item.count);
-      
-            setChartData({
-              labels: labels,
-              datasets: [
-                {
-                  data: counts,
-                  label: 'Intereses por zona',
-                  backgroundColor: [
-                    '#FF6384',
-                    '#36A2EB',
-                    '#FFCE56',
-                    '#FF6384',
-                    '#36A2EB',
-                    '#FFCE56'
-                  ],
-                  hoverBackgroundColor: [
-                    '#FF6384',
-                    '#36A2EB',
-                    '#FFCE56',
-                    '#FF6384',
-                    '#36A2EB',
-                    '#FFCE56'
-                  ]
-                }
-              ]
-            });
-          } catch (error) {
-            console.log(error);
-          }
-        };
-      
-        fetchData();
-      }, []);
-      
-    const options = {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            position: 'top',
-            labels: {
-              font: {
-                size: 25,
-                family: 'Arial',
-                weight: 'bold',
-                color: 'white'
-              }
-            }
-          }
-        },
-        scales: {
-          y: {
-            title: {
-              display: true,
-              text: 'Cantidad de usuarios'
-            },
-            ticks: {
-                beginAtZero: true,
-                stepSize: 1
-                }
-                },
-            x: {
-                title: {
-                display: true,
-                text: 'Mes'
-            },
-                ticks: {
-            autoSkip: false
-            }
-            }
-        }
     };
+    fetchData();
+  }, []);
 
-    return (
-        <div>
-            <h1>Usuarios por zona</h1>
-            <Chart
-                type='bar'
-                width={800}
-                height={450}
-                options={options}
-                data={getChartData}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <h1>Usuarios por zona</h1>
+      <Chart type="line" width={900} height={520} options={options} data={getChartData} />
+    </div>
+  );
 };
 
 export default Dashboard;
